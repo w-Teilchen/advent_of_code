b61666edc5954e2e5b5c44afef250b17bf28c189
Sören Sanders <soeren.sanders@worldiety.de>
2025-12-06 11:50:03 +0100

    Adapt justfile for my folder structure and add template file

---
+++ 2025/template.rs
@@ -0,0 +1,19 @@
+use lib::*;
+
+const INPUT: &str = include_str!("../input");
+const EXAMPLE: &str = include_str!("../example");
+
+fn main() {
+    assert_eq!(part1(EXAMPLE), 0);
+    println!("Part 1: {}", part1(INPUT));
+    assert_eq!(part2(EXAMPLE), 0);
+    println!("Part 2: {}", part2(INPUT));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
